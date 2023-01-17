aaf5e9b7465ef127a07d1215228a14e9da0f61a5
AjaySaini1999 <arayansaini1999@gmail.com>
2023-01-18 03:00:23 +0530

    Initial commit

---
+++ src/Demo_Class_2.java
@@ -0,0 +1,5 @@
+public class Demo_Class_2 {
+    public static void main(String[] args){
+        System.out.println("Demo Class 2");
+    }
+}
---
+++ src/Demo_Class_3.java
@@ -0,0 +1,11 @@
+public class Demo_Class_3 {
+        void sum(){
+            int a=4,b=4,c=9;
+
+            System.out.println(a+b+c);
+        }
+    public static void main(String[] args){
+            Demo_Class_3 demo= new Demo_Class_3();
+            demo.sum();
+    }
+}
