1a80be5a1d8976425ffcba50fb1fc03fa0c0ed2b
Praveen <praveenrajput171@gmail.com>
2023-01-24 00:01:49 +0530

    Demo Commit 1

--- src/Demo_Class_3.java
+++ src/Demo_Class_3.java
@@ -2,7 +2,7 @@ public class Demo_Class_3 {
         void sum(){
             int a=4,b=4,c=9;
 
-            System.out.println(a+b+c);
+            System.out.println(a+b+c+"Demo");
         }
     public static void main(String[] args){
             Demo_Class_3 demo= new Demo_Class_3();
