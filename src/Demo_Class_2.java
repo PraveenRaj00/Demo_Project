5bad28b3f490c2f00efe396fc44b33ebb5154fe6
Praveen <praveenrajput171@gmail.com>
2023-01-25 01:47:31 +0530

    Demo Commit 1

--- src/Demo_Class_2.java
+++ src/Demo_Class_2.java
@@ -2,6 +2,6 @@ public class Demo_Class_2 {
     public static void main(String[] args){
         double number=200,Percent=17,result;
         result = number*Percent/100;
-        System.out.println(result);
+        System.out.println("Change"+result);
     }
 }
